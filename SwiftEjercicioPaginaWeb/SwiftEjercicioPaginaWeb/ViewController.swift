9b4cd12314ad3b1b821242dbc7a8dabf38f9fbd8
jorgeSV <amk114@gmail.com>
2014-11-29 17:34:48 +0100

    Creación de proyecto

---
+++ SwiftEjercicioPaginaWeb/SwiftEjercicioPaginaWeb/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SwiftEjercicioPaginaWeb
+//
+//  Created by jorgeSV on 29/11/14.
+//  Copyright (c) 2014 JorgeSV. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
